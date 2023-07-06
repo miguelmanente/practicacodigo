d4dd0aba267ede55e2f0e92cc068d4c60a3f2ee2
Miguel Manente <manente2011@gmail.com>
2023-07-06 11:00:44 -0300

    EjercicioJs

---
+++ SumaMediaNumeros/js/index.js
@@ -0,0 +1,25 @@
+/*
+Utilizando un bucle, mostrar la suma y la media de los numeros introducidos
+hasta introducir un numero negativo y ahí mostrar el resultado
+*/
+'use strict'
+
+let suma = 0;
+let cuenta = 0;
+
+do{
+    let numero = parseInt(prompt("Ingrese números, para terminar ingrese un número negativo",0));
+
+    if(isNaN(numero)){
+        numero = 0;
+    } else if(numero > 0){
+        suma += numero;
+        cuenta++;
+    }
+}while(numero >= 0);
+
+alert("La suma de todos los números ingresados es: "+suma);
+alert("La media de todos los números ingresados es: "+(suma/cuenta));
+
+
+
