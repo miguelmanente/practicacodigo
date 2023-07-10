e3a70c12382ae947ce20c7af3237e711262d94d5
Miguel Manente <manente2011@gmail.com>
2023-07-10 17:29:41 -0300

    EjerciciosJS

---
+++ MuestraNumeros/js/index.js
@@ -0,0 +1,9 @@
+/* Hacer un programa que muestre todos los numeros entre dos numeros introducidos por el usuario*/
+
+let numero1 = parseInt(prompt("Introduce el primer numero", 0));
+let numero2 = parseInt(prompt("Introduce el segundo numero", 0));
+
+document.write("<h1>De "+numero1+" a "+numero2+" están estos numeros:</h1>")
+for(var i = numero1; i <= numero2; i++){
+	document.write(i+" ");
+}
